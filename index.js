712ef65e5c9e56c6304f186cd0bb0dd05d3b7103
Prashanth <prashanth@Prashanths-MacBook-Air.local>
2025-10-05 00:39:00 -0400

    Updated index.js

--- index.js
+++ index.js
@@ -6,18 +6,19 @@ async function main() {
   const readmeTemplate = (
     await fs.readFile(path.join(process.cwd(), "./README.template.md"))
   ).toString("utf-8");
+
+  const bb_quote = await (
+    await fetch("https://api.breakingbadquotes.xyz/v1/quotes")
+  ).json();
+
+  const readme = readmeTemplate
+    .replace("{bb_quote}", bb_quote.quote)
+    .replace(
+      "{bb_character}",
+      `- ${bb_quote.author}`
+    );
+
+  await fs.writeFile("README.md", readme);
 }
+
 main();
-
-const bb_quote = await (
-  await fetch("https://api.breakingbadquotes.xyz/v1/quotes")
-).json();
-
-const readme = readmeTemplate
-  .replace("{bb_quote}", bb_quote.quote)
-  .replace(
-    "{bb_character}",
-    `- ${bb_quote.author}`
-  );
-
-await fs.writeFile("README.md", readme);
